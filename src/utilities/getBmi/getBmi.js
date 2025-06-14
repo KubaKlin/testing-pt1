e51f62c72f8391620ebb5bcffb3d08c52a60886b
Kuba Klin <poczta@kubaklin.pl>
2025-06-14 09:44:56 +0200

    fix: get bmi function fix

--- src/utilities/getBmi/getBmi.js
+++ src/utilities/getBmi/getBmi.js
@@ -1,21 +1,25 @@
 export const getBmi = (weight, height) => {
-
   if (typeof weight !== 'number' || typeof height !== 'number') {
     return 'Weight and height must be numbers';
   }
   
   if (weight <= 0 || height <= 0) {
-    return 'Weight and height must be positive numbers'
+    return 'Weight and height must be positive numbers';
   }
+  
   const bmi = weight / (height * height);
 
   if (bmi < 18.5) {
     return 'underweight';
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return 'normal weight';  
-  } else if (bmi >= 25 && bmi <= 29.9) {
-    return 'overweight';
-  } else {
-    return 'obese';
   }
+  
+  if (bmi < 25) {
+    return 'normal weight';
+  }
+  
+  if (bmi < 30) {
+    return 'overweight';
+  }
+  
+  return 'obese';
 };
